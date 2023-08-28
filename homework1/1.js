9e47368ff6db051ec90adbde5b0c4aaab6262b60
Vladimir <maostyle@mail.ru>
2023-08-28 20:30:34 +0300

    complete 1st task

--- homework1/1.js
+++ homework1/1.js
@@ -13,4 +13,6 @@
 Уточнение: пользователь всегда вводит корректное число.
 */
 
-// Здесь пишем решение, данный комментарий необходимо стереть.
+const degreesCelsius = prompt("Введите температуру в градусах Цельсия:  ");
+const degreesFahrenheit = (9 / 5) * degreesCelsius + 32;
+alert(`Цельсий: ${degreesCelsius}, Фаренгейт: ${degreesFahrenheit}`);
